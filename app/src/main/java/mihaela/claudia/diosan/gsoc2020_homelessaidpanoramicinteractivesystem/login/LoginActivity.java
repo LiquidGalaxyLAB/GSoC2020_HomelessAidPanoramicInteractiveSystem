4de67d41c51d3d16de6e1f0a4143ef343737f887
ClaudiaMihaelaDiosan <mcd1591@gmaile.com>
2020-06-01 18:07:18 +0200

    Splash Screen and Intro Activity

---
+++ app/src/main/java/mihaela/claudia/diosan/gsoc2020_homelessaidpanoramicinteractivesystem/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package mihaela.claudia.diosan.gsoc2020_homelessaidpanoramicinteractivesystem.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import mihaela.claudia.diosan.gsoc2020_homelessaidpanoramicinteractivesystem.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
