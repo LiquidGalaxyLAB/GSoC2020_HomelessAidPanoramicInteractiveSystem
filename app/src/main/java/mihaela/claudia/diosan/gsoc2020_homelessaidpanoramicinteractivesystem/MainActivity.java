d5f6084cf67955e1ad1fe2911b28109c0bc9f73d
ClaudiaMihaelaDiosan <mcd1591@gmaile.com>
2020-06-01 16:59:49 +0200

    Initial commit

---
+++ app/src/main/java/mihaela/claudia/diosan/gsoc2020_homelessaidpanoramicinteractivesystem/MainActivity.java
@@ -0,0 +1,14 @@
+package mihaela.claudia.diosan.gsoc2020_homelessaidpanoramicinteractivesystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
